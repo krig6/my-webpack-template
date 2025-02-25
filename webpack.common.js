8891800bfc8078be3edca6e0c7da142c304484a9
krig6 <ej.ldsm@gmail.com>
2025-02-25 14:53:23 +0800

    feat: Add webpack.common.js with basic configuration
    - Added webpack.common.js with entry, output, plugins, and module rules for handling CSS and image assets

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js')
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/template.html',
+      filename: 'index.html',
+      title: 'Webpack Template'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loarder', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|svg|fig)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext]'
+        }
+      }
+    ],
+  },
+}
+
